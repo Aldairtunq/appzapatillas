0002cde1ba2c2194a61f26e9b82dd6f036668d33
Aldairtunq <201912945b@utea.edu.pe>
2022-12-23 11:25:06 -0500

    commit home page avanze

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,6 +1,7 @@
 import 'package:appzapatillas/models/data.dart';
 import 'package:appzapatillas/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePageShoes extends StatefulWidget {
   const HomePageShoes({super.key});
@@ -47,52 +48,98 @@ class _HomePageShoesState extends State<HomePageShoes> {
                   controller: _pagecontroller,
                   itemBuilder: (context, index) {
                     final shoes = listShoes[index];
-                    return Container(
-                        margin: const EdgeInsets.symmetric(vertical: 30),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(36),
-                          color: Colors.white, //2:1
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20.0,
-                            vertical: 40,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Text(
-                                shoes.category,
-                                style: const TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                shoes.name,
-                                style: const TextStyle(
-                                  fontSize: 28,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w800,
+                    final listTitle = shoes.category.split('');
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 30.0),
+                      child: LayoutBuilder(builder: (context, Constraints) {
+                        return Container(
+                            margin: const EdgeInsets.symmetric(vertical: 30),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(36),
+                              color: Colors.white,
+                            ),
+                            child: Stack(
+                              clipBehavior: Clip.none, //4:03
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 20.0,
+                                    vertical: 40,
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      Text(
+                                        shoes.category,
+                                        style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      const SizedBox(
+                                        height: 8,
+                                      ),
+                                      Text(
+                                        shoes.name,
+                                        style: const TextStyle(
+                                          fontSize: 28,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w800,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 4,
+                                      ),
+                                      Text(
+                                        shoes.price,
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 4,
+                                      ),
+                                      Text(
+                                        shoes.price,
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 4,
+                                      ),
+                                      FittedBox(
+                                        child: Text(
+                                          '${listTitle[0]} \ ${listTitle[1]}',
+                                          style: const TextStyle(
+                                            color: Colors.grey,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 4,
-                              ),
-                              Text(
-                                shoes.price,
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
+                                Positioned(
+                                  top: Constraints.maxHeight * 0.2,
+                                  left: Constraints.maxWidth * 0.05,
+                                  right: -Constraints.maxWidth * 0.16,
+                                  bottom: Constraints.maxHeight * 0.2,
+                                  child: Image(
+                                    image: AssetImage(
+                                      shoes.listImage[0].image,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ));
+                              ],
+                            ));
+                      }),
+                    );
                   }))
         ],
       ),
