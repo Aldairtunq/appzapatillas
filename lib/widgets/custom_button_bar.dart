506669f85b4820fadada6b795ed1373e7b035b51
Aldairtunq <201912945b@utea.edu.pe>
2022-12-23 00:53:36 -0500

    creacion de las carpetas

---
+++ lib/widgets/custom_button_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CustomBottonBar extends StatelessWidget {
+  const CustomBottonBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      decoration: BoxDecoration(
+          color: Colors.blue, borderRadius: BorderRadius.circular(40)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.house,
+                size: 36,
+              ),
+            ),
+            Container(
+              height: double.infinity,
+              width: 50,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: Center(
+                child: Container(
+                  height: 25,
+                  width: 25,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
